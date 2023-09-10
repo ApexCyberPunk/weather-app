571e6b25e6633c008e34f90e3e1b2a0db62660bb
snovacain <61154743+snovacain@users.noreply.github.com>
2023-09-10 14:25:26 -0700

    le git it

--- weather.js
+++ weather.js
@@ -12,12 +12,19 @@ let getWeather = () => {
 if(cityValue.length == 0) {
     result.innerHTML = `<h3> Please enter a city name</h3>`;
 } else {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
 
     fetch(url)
         .then((x) => x.json())
+        // if city name is valid
+
         .then((y) => {
             console.log(y)
+            console.log(data.weather)
+        })
+
+        .catch(() => {
+            result.innerHTML = `<h3>City not found</h3>`
         })
 }
 };
